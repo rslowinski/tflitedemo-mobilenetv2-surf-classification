ef91242c2db924e88dc4ab41fad5d7b65a3d3d7e
Radek Slowinski <rslowinski97@gmail.com>
2019-05-05 20:42:07 +0200

    Camera setup

--- app/src/main/java/com/radslow/tflitedemo/MainActivity.kt
+++ app/src/main/java/com/radslow/tflitedemo/MainActivity.kt
@@ -1,12 +1,94 @@
 package com.radslow.tflitedemo
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!canUseCamera()) {
+            requestCameraPermissions()
+        } else {
+            setupCamera()
+        }
+
+    }
+
+    private fun requestCameraPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CAMERA),
+            REQUEST_CAMERA_CODE
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun setupCamera() {
+        camera.addPictureTakenListener {}
+
+        capturePhoto.setOnClickListener {
+            camera.capture()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (REQUEST_CAMERA_CODE == requestCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setupCamera()
+            } else {
+                Toast.makeText(this, "App needs camera in order to work.", Toast.LENGTH_LONG).show()
+                requestCameraPermissions()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onResume() {
+        super.onResume()
+        if (canUseCamera()) {
+            camera.start()
+        }
+    }
+
+    override fun onPause() {
+        if (canUseCamera()) {
+            camera.stop()
+        }
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        if (canUseCamera()) {
+            camera.destroy()
+        }
+        super.onDestroy()
+    }
+
+    private fun canUseCamera() =
+        ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+    companion object {
+        private const val REQUEST_CAMERA_CODE = 1
     }
 }
